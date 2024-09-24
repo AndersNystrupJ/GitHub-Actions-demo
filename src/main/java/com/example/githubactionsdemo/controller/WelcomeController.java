067c888a29aafe0f63e4b0586b2364fd36737543
AndersNystrupJ <anje0008@stud.kea.dk>
2024-09-24 14:14:36 +0200

    Test commit

--- src/main/java/com/example/githubactionsdemo/controller/WelcomeController.java
+++ src/main/java/com/example/githubactionsdemo/controller/WelcomeController.java
@@ -2,10 +2,11 @@ package com.example.githubactionsdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
-    @GetMapping("")
+    @RequestMapping("/welcome")
     public String welcomeString() {
         return "welcome";
     }
